16ef1960d3b43a6d58efd8794c49cc42308277bb
nemanjacvetojevic <nemanjacvetojevicdev@gmail.com>
2020-08-01 19:32:20 +0200

    inital commit and first part finished for Ravenous restaurant app from codecademy

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './App.css'
+import { BusinessList } from '../BusinessList/BusinessList';
+import { SearchBar } from '../SearchBar/SearchBar'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
